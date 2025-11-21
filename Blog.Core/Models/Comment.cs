e5ef21f58f3b04879584445f6e3ae48b325b800a
Michael Lau <lau0@sheridancollege.ca>
2025-11-21 18:43:43 -0500

    Add Post and Comment domain models with EF annotations

---
+++ Blog.Core/Models/Comment.cs
@@ -0,0 +1,31 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogAPI.Core.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int PostId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(150)]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(1000)]
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        // Navigation property back to Post
+        public Post? Post { get; set; }
+    }
+}
---
+++ Blog.Core/Models/Post.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogAPI.Core.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Content { get; set; } = string.Empty;
+
+        // Assignment says author is hardcoded as "admin"
+        [Required]
+        public string Author { get; set; } = "admin";
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedDate { get; set; }
+
+        // One Post -> Many Comments
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
